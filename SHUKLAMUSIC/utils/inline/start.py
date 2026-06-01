a988b521c32b66f81b6345c38dbe013e3ea300ee
KRISH X STAR MUSIC WORLD <krishbairag@gmail.com>
2026-06-01 22:26:20 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -1,4 +1,4 @@
-# -----------------------------------------------
+i# -----------------------------------------------
 # 🔸 StrangerMusic Project
 # 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
 # 📅 Copyright © 2022 – All Rights Reserved
@@ -39,7 +39,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_10"], callback_data="shiv_Shashank"),
+            InlineKeyboardButton(text=_["S_B_10"], callback_data="KRISH_HACKER_OWNER"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
